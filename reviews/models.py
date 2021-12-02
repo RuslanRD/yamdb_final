a3c99e0877c14b66295a0ad53c7abce59b623deb
RuslanRD <r.ruslan.d@yandex.kz>
2021-12-03 02:03:58 +0600

    third. update pep8 v17.

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,8 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from users.models import User
 
 from .validators import title_year_validator
-
-User = get_user_model()
 
 
 class Category(models.Model):
